19a2deb25b0d6a13d744b0048d5e9801f81c068e
Priti Jagtap <pritidjagtap1@gmail.com>
2025-06-29 23:23:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import './App.css';
+let name = "Pritii"
+function App() {
+  return (
+    <>
+    <nav>
+      <li>Home</li>
+      <li>About</li>
+      <li>Contact</li>
+      </nav>
+     <div className="container">
+      <h1>Hello {name}</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Animi, illo officiis sed repellendus consequuntur ipsa fuga 
+        modi odio temporibus aliquam nam nostrum? Beatae dolore ipsam
+         pariatur ullam odio possimus veniam?</p>
+     </div>
+
+      </>
+
+
+
+
+  );
+}
+
+export default App;
